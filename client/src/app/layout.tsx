083312d67ee75d977b066935699a3400ac9b2018
Osama <shaikhosama499@gmail.com>
2023-08-13 22:19:25 +0530

    build(app): layout of the project created

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'TaskTrackr',
+  description: 'Created for QuickSell Interview',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{"backgroundColor":"#3d3d4e"}} className={inter.className}>{children}</body>
+    </html>
+  )
+}
