e9d37807c49d34762b00a6dca9f7be3ca9a40a3b
Osama <shaikhosama499@gmail.com>
2023-08-15 13:33:00 +0530

    feat(app): edit component created for editing task

---
+++ client/src/app/components/Edit.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import {AiOutlineEdit } from "react-icons/ai";
+import Modal from './modal';
+import { TodoObject } from '@/types/models';
+interface TaskProps {
+    todo: TodoObject;
+     onDescriptionUpdated: (taskId: number, newDescription: string) => void;
+}
+const Edit: React.FC<TaskProps> = ({todo , onDescriptionUpdated}) => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [taskToEdit, setTaskToEdit] = useState<string>(todo.description);
+
+     const updateDescription = async e => {
+    e.preventDefault();
+    try {
+      const body = {description: taskToEdit };
+      const response = await fetch(
+        `http://localhost:4000/update/${todo.server_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
+        }
+        );
+        //  setTaskToEdit(todo.description); 
+        setModalOpen(false); // Close the modal
+        onDescriptionUpdated(todo.server_id, taskToEdit);
+     
+    } catch (err:any) {
+      console.error(err.message);
+    }
+  };
+  return (
+      <>
+            <button onClick={() =>  setModalOpen(true) } className='cursor-pointer mx-2'  >
+              <AiOutlineEdit />
+            </button>
+      <Modal  modalOpen={modalOpen} setModalOpen={setModalOpen} >
+    <form onSubmit={updateDescription}>
+          <h3 className='font-bold text-lg'>Edit task</h3>
+          <div className='modal-action'>
+            <input
+              value={taskToEdit}
+              onChange={(e) => setTaskToEdit(e.target.value)}
+              type='text'
+              placeholder='Type here'
+              className='input input-bordered w-full'
+            />
+                      <button type='submit' className='btn' >
+              Submit
+            </button>
+          </div>
+        </form>     
+        </Modal></>
+  )
+}
+
+export default Edit
